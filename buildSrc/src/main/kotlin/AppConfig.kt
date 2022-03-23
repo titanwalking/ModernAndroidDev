cc3b354b68f36bb8cf9a24d1c2709d2f2d1d2094
Alican Korkmaz <akorkmaz@pm.me>
2022-03-23 21:02:06 +0300

    fix: Removed unnecessary constants from AppConfig

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -4,8 +4,6 @@ object AppConfig {
     const val compileSdkVersion = 32
     const val minSdkVersion = 21
     const val targetSdkVersion = 32
-    const val buildToolsVersion = "30.0.3"
     const val versionCode = 1
     const val versionName = "1.0"
-    const val baseUrl = "https://api.github.com/"
 }
